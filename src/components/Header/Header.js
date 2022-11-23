a371d7fdba2fd3b0bdfeb2e458c28fcc4f2c4783
Jakub <jakub.jaszcz538@gmail.com>
2022-11-23 21:09:12 +0100

    Code added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import Button from '../UI/Button/Button'
+import React from 'react';
+import Button from '../UI/Button/Button';
+import classes from './Header.module.css';
 
 const Header = () => {
   return (
-    <header><h1>Welcome to Poland</h1></header>
+    <header className={classes.header}><h1 className={classes.header__heading}>Welcome to Poland</h1></header>
    
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [showFinder,setShowFinder]=useState(false);
+
+  const clickHandler=()=>{
+    setShowFinder(prevState=>!prevState);
+  }
+  
   return (
-    <div className='navbar'>
-        <h3>visit<span className='font-poland'>Poland</span>.pl</h3>
-        <nav className='navbar__links'>
-          <a href="#">Places to go</a>
-          <a href="#">Magazine</a>
-          <a href="#">Tickets</a>
-          <select>
-            <option>EN</option>
-            <option>DE</option>
-            <option>PL</option>
-          </select>
-          <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+    <div className={classes.navbar}>
+        <h3 className={classes.navbar__logo}>visit
+          <span className={classes['logo-italic']}>
+            Poland
+          </span>.pl
+        </h3>
+        <nav className={classes.navbar__links}>
+          <a href="#" className={classes.navbar__link}>Places to go</a>
+          <a href="#" className={classes.navbar__link}>Magazine</a>
+          <a href="#" className={classes.navbar__link}>Tickets</a>
+          <input className={!showFinder?classes['finder'] :''} />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className={classes.magnifier} onClick={clickHandler}/>
         </nav>
     </div>
   )
